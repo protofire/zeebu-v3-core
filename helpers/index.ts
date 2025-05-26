59317775760aa9dbe0d05958fd79b218f1a3ecc5
Erion Barasuol <erionrb@gmail.com>
2025-05-26 15:04:09 -0300

    new script for token get caps

--- helpers/index.ts
+++ helpers/index.ts
@@ -1,3 +1,12 @@
 export * from './constants';
 export * from './types';
 export * from './contracts-helpers';
+
+import { loadTasks } from './load-tasks';
+
+const TASK_FOLDERS = ['../tasks/misc'];
+(async () => {
+  for (const folder of TASK_FOLDERS) {
+    await loadTasks(folder);
+  }
+})();
